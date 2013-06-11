cc4e6d2c66ac82c3954a890df708c0a4b8f12576
Mikis Seth Sørensen <mss@statsbiblioteket.dk>
2013-06-11 09:16:03 +0200

    Updated sh scripts to run on macs.

--- src/main/resources/script/migration/migrateCollectionSettings.sh
+++ src/main/resources/script/migration/migrateCollectionSettings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-SCRIPT_PATH=$(dirname $(readlink -f $0))
+SCRIPT_PATH=$(dirname $(perl -e "use Cwd 'abs_path';print abs_path('$0');"))
 
 xsltproc "$SCRIPT_PATH/collection2repositorysettings.xsl" CollectionSettings.xml > RepositorySettings.xml
